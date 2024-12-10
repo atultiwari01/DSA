1d36263f85bcbd6b4070339d667a22bdd9286d6a
Atul Tiwari <129764279+atultiwari01@users.noreply.github.com>
2024-12-11 02:54:34 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,37 @@
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        // Traverse through all array elements
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 45, 88, 11};
+        System.out.println("array:");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("Sortarray:");
+        printArray(arr);
+    }
+}
