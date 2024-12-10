49437a7e90350c65614c79fe78fee43bed558bab
Atul Tiwari <129764279+atultiwari01@users.noreply.github.com>
2024-12-11 03:02:58 +0530

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,8 +1,6 @@
 public class SelectionSort {
     public static void selectionSort(int[] arr) {
         int n = arr.length;
-
-        // Traverse through all array elements
         for (int i = 0; i < n - 1; i++) {
             int min = i;
             for (int j = i + 1; j < n; j++) {
